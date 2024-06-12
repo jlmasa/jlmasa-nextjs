a05388c210c6b65b80a191eeed9f2cf9f200fb5a
john masa <johnlorenz.masa@gmail.com>
2024-06-12 16:56:16 +0800

    adjustment on visuals

--- src/components/education.js
+++ src/components/education.js
@@ -6,30 +6,24 @@ const Education = () => {
   return (
     <section>
       <div className="flex items-center justify-center min-h-screen">
-        <div className="grid lg:grid-cols-2 grid-cols-1">
-          <div className="flex flex-col items-start justify-center px-20">
-            <h1 className="text-3xl py-5 text-green-400">HELLO AND WELCOME</h1>
-            <h1 className="text-3xl font-bold ">
-              I&apos;m John Lorenz Eldon R. Masa
-            </h1>
-            <h1 className="text-2xl text-justify py-5">
-              I&apos;m a Graduate of Bachelor of Science in Information
-              Technology. And ever since I first got my own computer unit at the
-              age of 8, I was able to Enjoy a lot of things and It came to a
-              point where I ask myself how does a computer works? How does a
-              computer understand our inputs and that is where I decided to
-              pursue this path of Technology. I&apos;m Also a former Member of
-              the student body back in my college years. Where I was Elected As
-              Public Relations Officer back in 2016 for a year mainly focus on
-              being the voice of the students. I was also Elected As Vice
-              President of the Student body back in 2017 for a year focus on
-              creating events for the students to enjoy their stay during their
-              college years.
-            </h1>
-          </div>
-          <div className="flex flex-col items-center justify-center p-14">
-            <Image src={face1} className="rounded-xl" alt="image" />
-          </div>
+        <div className="flex flex-col items-center justify-center px-20">
+          <h1 className="text-3xl py-2 text-green-400">HELLO AND WELCOME</h1>
+          <h1 className="text-3xl font-bold ">
+            I&apos;m John Lorenz Eldon R. Masa
+          </h1>
+          <h1 className="text-2xl text-justify py-10 w-[60vw]">
+            I&apos;m a Graduate of Bachelor of Science in Information
+            Technology. And ever since I first got my own computer unit at the
+            age of 8, I was able to Enjoy a lot of things and It came to a point
+            where I ask myself how does a computer works? How does a computer
+            understand our inputs and that is where I decided to pursue this
+            path of Technology. I&apos;m Also a former Member of the student
+            body back in my college years. Where I was Elected As Public
+            Relations Officer back in 2016 for a year mainly focus on being the
+            voice of the students. I was also Elected As Vice President of the
+            Student body back in 2017 for a year focus on creating events for
+            the students to enjoy their stay during their college years.
+          </h1>
         </div>
       </div>
     </section>
