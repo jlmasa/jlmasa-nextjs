185103b2996b55253bedb93627d14cf7426fee44
john masa <johnlorenz.masa@gmail.com>
2024-06-12 16:09:43 +0800

    adjustment on text

--- src/components/landing.js
+++ src/components/landing.js
@@ -6,7 +6,7 @@ const Landing = () => {
   return (
     <section>
       <div className="flex flex-col items-center justify-center min-h-screen">
-        <h1 className="text-white text-5xl pb-10">Hello!</h1>
+        <h1 className="text-5xl pb-10">Hello!</h1>
         <div className="bg-gradient-to-r from-blue-500 to-red-500 bg-clip-text text-transparent pb-10">
           <h1 className="lg:text-8xl text-5xl">I am JL masa,</h1>
           <h1 className="lg:text-8xl text-5xl">A Fullstack Developer</h1>
