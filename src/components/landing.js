09a6a404a9b24a37a339a2edcfe0680e7b004667
john masa <johnlorenz.masa@gmail.com>
2024-06-13 13:18:35 +0800

    center text

--- src/components/landing.js
+++ src/components/landing.js
@@ -7,7 +7,7 @@ const Landing = () => {
     <section>
       <div className="flex flex-col items-center justify-center min-h-screen">
         <h1 className="text-5xl pb-10">Hello!</h1>
-        <div className="bg-gradient-to-r from-blue-500 to-red-500 bg-clip-text text-transparent pb-10">
+        <div className="bg-gradient-to-r from-blue-500 to-red-500 bg-clip-text text-transparent pb-10 text-center">
           <h1 className="lg:text-8xl text-5xl">I am JL masa,</h1>
           <h1 className="lg:text-8xl text-5xl">A Fullstack Developer</h1>
         </div>
